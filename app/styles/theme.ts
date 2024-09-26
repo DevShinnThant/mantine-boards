8ded136307ee0069f72762672b5b5d160ca94819
shinnthant2004 <shinthantequi@gmail.com>
2024-09-26 16:54:19 +0630

    style: add dark colors

--- app/styles/theme.ts
+++ app/styles/theme.ts
@@ -14,6 +14,18 @@ export const theme = createTheme({
       "hsl(263.5 67.4% 34.9%)",
       "hsl(261.2 72.6% 22.9%)",
     ],
+    dark: [
+      "#C1C2C5",
+      "#A6A7AB",
+      "#909296",
+      "#5c5f66",
+      "#373A40",
+      "#2C2E33",
+      "#25262b",
+      "#1A1B1E",
+      "#141517",
+      "#101113",
+    ],
   },
   primaryColor: "gray",
   headings: {
